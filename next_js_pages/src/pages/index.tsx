26f0b76756513ca237cd50d69295da0af3e41696
Edmundo Gonzalez <edmundo.gonzalez@coppel.com>
2025-06-11 17:44:11 -0600

    Actualizacion de ejemplo a version 1.1.2

--- next_js_pages/src/pages/index.tsx
+++ next_js_pages/src/pages/index.tsx
@@ -4,6 +4,7 @@ import { useEffect, useState } from "react";
 export default function Home() {
   const [authService, setAuthService] = useState<AuthService>();
   const [userData, setUserData] = useState<Map<String, String>>();
+  const [token, setToken] = useState<string>();
   const [data, setData] = useState(null);
 
   function handleClick() {
@@ -15,13 +16,13 @@ export default function Home() {
   }
 
   const fetchData = async (path: string) => {
-    console.log(authService?.getAccessToken());
+    console.log(await authService?.getAccessToken());
 
     const url = process.env.NEXT_PUBLIC_APIGEE_URL + path + '/protected'
     const res = await fetch(url, {
       method: 'GET',
       headers: {
-        'Authorization': 'Bearer ' + authService?.getAccessToken()
+        'Authorization': 'Bearer ' + await authService?.getAccessToken()
       },
     });
 
@@ -32,36 +33,55 @@ export default function Home() {
   useEffect(() => {
     setAuthService(AuthService.getInstance());
     setUserData(AuthService.getInstance()?.getUserData());
+    AuthService.getInstance().getAccessToken().then(
+      (token) => setToken(token)
+    );
   }, []);
 
   return (
     <div>
-      <button
-        onClick={handleClick}
-        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full"
-      >
-        {authService?.isAuthenticated() ? "LogOut" : "Login"}
-      </button>
-      <button
-        onClick={() => fetchData('entraID')}
-        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full"
-      >
-        EntraID
-      </button>
-      <button
-        onClick={() => fetchData('auth0')}
-        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full"
-      >
-        Auth0
-      </button>
-      <button
-        onClick={() => fetchData('auth0-entraID')}
-        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full"
-      >
-        EntraID & Auth0
-      </button>
+      <div>
+        <button
+          onClick={handleClick}
+          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full"
+        >
+          {authService?.isAuthenticated() ? "LogOut" : "Login"}
+        </button>
+        {authService?.isAuthenticated() && <button
+          onClick={() => authService?.refreshAccessToken()}
+          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full"
+        >
+          Refresh Token
+        </button>}
+        {authService?.isAuthenticated() && <button
+          onClick={() => authService?.loginDeleteAccount("/")}
+          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full"
+        >
+          Delete Account
+        </button>}
+      </div>
+      <div>
+        <button
+          onClick={() => fetchData('entraID')}
+          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full"
+        >
+          EntraID
+        </button>
+        <button
+          onClick={() => fetchData('auth0')}
+          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full"
+        >
+          Auth0
+        </button>
+        <button
+          onClick={() => fetchData('auth0-entraID')}
+          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full"
+        >
+          EntraID & Auth0
+        </button>
+      </div>
       {/* <div>{userData && JSON.stringify(userData)}</div> */}
-      <div>{authService && authService.getAccessToken()}</div>
+      <pre>{authService && token && JSON.stringify(authService.getJwtPayload(token), undefined, 2)}</pre>
     </div>
   );
 }
